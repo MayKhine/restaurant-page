3ce7d373ddf41dd6637ac5309e460eb741c166aa
May <maykhine.ada@gmail.com>
2023-07-14 22:07:05 -0400

    update css for menu

--- src/menuContent.js
+++ src/menuContent.js
@@ -1,12 +1,12 @@
 const menuJson = require("./assets/menu.json");
 
 const menuList = [
-  "appetizer",
-  "mainCourse",
-  "soupSalads",
-  "specials",
-  "desserts",
-  "drinks",
+  "Appetizer",
+  "Main Course",
+  "Soup and Salads",
+  "Specials",
+  "Desserts",
+  "Drinks",
 ];
 
 // console.log("menuJSON:", menuJson);
@@ -129,12 +129,15 @@ const menuContent = () => {
   menuList.forEach((curSection) => {
     if (catogries[curSection]) {
       //create header
-      const menuSectionHeader = createHeader("h4", "menuSection");
+      const menuSection = createDiv("menuSection");
+      const menuSectionHeader = createHeader("h4", "menuSectionHeader");
       menuSectionHeader.textContent = curSection;
-      const menuSection = document.createElement("ul");
+      const menuSectionDetail = document.createElement("ul");
       // menuSection.className = curSection;
-      menuSectionHeader.appendChild(menuSection);
-      menuBody.appendChild(menuSectionHeader);
+      menuSection.appendChild(menuSectionHeader);
+      menuSection.appendChild(menuSectionDetail);
+
+      menuBody.appendChild(menuSection);
 
       catogries[curSection].forEach(
         (item) => {
@@ -144,12 +147,13 @@ const menuContent = () => {
           const menuItemIngredient = document.createElement("p");
           menuItemIngredient.textContent = item.ingredient;
           const menuItemPrice = document.createElement("p");
+          menuItemPrice.className = "itemPrice";
           menuItemPrice.textContent = item.price;
 
           //add item to header
           menuItem.appendChild(menuItemIngredient);
           menuItem.appendChild(menuItemPrice);
-          menuSection.appendChild(menuItem);
+          menuSectionDetail.appendChild(menuItem);
         }
 
         //add header to body
